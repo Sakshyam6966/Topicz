76c3f5d24ebe58eb386dea64c1be3d1c9e07c687
Not Sakshyam <88571629+Sakshyam6966@users.noreply.github.com>
2021-08-22 13:52:07 +0545

    Update support.js

--- src/commands/support.js
+++ src/commands/support.js
@@ -7,11 +7,11 @@ module.exports.run = async (client,message,args) => {
   let bots = await x.filter(a => a.ownerID == message.author.id || a.coowners.includes(message.author.id))
    const embed = new Discord.MessageEmbed()
    .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
-   .setDescription(`<a:yes:833101995723194437> **[Vcodez Support](https://vcodez.xyz)** <a:yes:833101995723194437>\n\n**Visit the official support chat for Vcodez where you can ask our Team members questions! You can also find one in our [web](https://vcodez.xyz)!**`)
+   .setDescription(`<a:yes:833101995723194437> **[Topicz Support](https://topicz.xyz)** <a:yes:833101995723194437>\n\n**Visit the official support chat for Topicz where you can ask our Team members questions! You can also find one in our [web](https://topicz.xyz)!**`)
    .setColor("#7289da")
          let button = new MessageButton()
   .setStyle('url')
-  .setURL('https://vcodez.xyz/dc') 
+  .setURL('https://topicz.xyz/dc') 
   .setLabel('Join our Support Server')
     .setEmoji('866089515993792522');
 
